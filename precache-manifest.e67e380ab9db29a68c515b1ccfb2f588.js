ee61fcd6bfddb5ed44859b462978cfd55d29b8a6
a.chernets <alex.chernets@almexoft.com>
2020-01-09 15:40:46 +0200

    Updates

---
+++ precache-manifest.e67e380ab9db29a68c515b1ccfb2f588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0f219cb6b51cadd6c69444b70ff18e",
+    "url": "/almex-constractor/index.html"
+  },
+  {
+    "revision": "44c66eb57d3fa2752633",
+    "url": "/almex-constractor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "baec3b223a9b1e2625c4",
+    "url": "/almex-constractor/static/js/2.ba28062d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/almex-constractor/static/js/2.ba28062d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44c66eb57d3fa2752633",
+    "url": "/almex-constractor/static/js/main.75587eab.chunk.js"
+  },
+  {
+    "revision": "9b35383b597793bd385a",
+    "url": "/almex-constractor/static/js/runtime-main.5a59df4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/almex-constractor/static/media/logo.5d5d9eef.svg"
+  }
+]);

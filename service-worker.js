b135bb7b3ba1c66c291fdf2c884af38d6272b1d5
a.chernets <alex.chernets@almexoft.com>
2020-05-07 12:02:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/almex-sign/precache-manifest.1b03f5d01a486b87391a2356c733f849.js"
+  "/almex-sign/precache-manifest.796c3f3ed89ac381e452da4ec311fb45.js"
 );
 
 self.addEventListener('message', (event) => {
